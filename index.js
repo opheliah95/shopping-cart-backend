f2c589b144f57eb1ded013b8746387b65de2b6a9
opheliah95 <62559892+opheliah95@users.noreply.github.com>
2024-08-06 13:34:39 +0100

    initial backend setup

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ app.get("/", (req,res)=> {
     res.send("Welcome to the online shop API")
 
 })
+// product route
+app.get("/products", (req,res)=> {
+    res.send([2,3,4])
+
+})
+
+const port = process.env.PORT || 5005;
 
 app.listen(5005, ()=>{
-    console.log("Server running on port 5000")
+    console.log(`Server running on port ${port}`)
 })
